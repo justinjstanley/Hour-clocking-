4f36fe239460e7167e14ae2d70ba3f9ee22fc94b
justinjstanley <justinjstanley@gmail.com>
2025-10-17 11:20:39 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'simple-hours-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const { request } = e;
+  if (request.method === 'GET' && new URL(request.url).origin === location.origin) {
+    e.respondWith(
+      caches.match(request).then(cached => cached || fetch(request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(request, copy));
+        return resp;
+      }).catch(() => cached))
+    );
+  }
+});
